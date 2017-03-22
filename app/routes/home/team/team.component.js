29d563b5ac46a8ff71ede0118cf84c3ac280b9a6
daniel_leszcz <danielxleszczynski@gmail.com>
2017-03-22 12:25:21 +0100

    Add range component

--- app/routes/home/team/team.component.js
+++ app/routes/home/team/team.component.js
@@ -6,17 +6,16 @@ export default class Team extends PureComponent {
     data: PropTypes.object.isRequired,
   };
 
-
   render() {
-
     const spanStyle = {
-      color: 'blue',
-      backgroundImage: 'url(' + this.props.data.get('crestUrl') + ')',
+      backgroundImage: 'url(' + this.props.data.get('crestUrl') + ')'
     };
+
     return (
       <li className="team">
         <span className="team-logo" style={spanStyle}></span>
-        <span className="team-name">{this.props.data.get('name')}</span>
+        <span className="team-name">{this.props.data.get('name')},</span>
+        <span className="team-value">{this.props.data.get('squadMarketValue')}</span>
       </li>
     );
   }
--- app/routes/home/teamsList/teamsList.component.js
+++ app/routes/home/teamsList/teamsList.component.js
@@ -3,12 +3,23 @@ import { FormattedMessage } from 'react-intl';
 
 import messages from './teamsList.messages';
 import Team from '../team/team.component';
-
+import InputRange from 'react-input-range';
 
 export default class TeamsList extends PureComponent {
   static propTypes = {
     items: PropTypes.object.isRequired,
   };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: {
+        min: 100,
+        max: 200
+      }
+    };
+  }
+
 
   render() {
     return (
@@ -16,6 +27,14 @@ export default class TeamsList extends PureComponent {
         <h2 className="teams-list__title">
           <FormattedMessage {...messages.title} />:
         </h2>
+
+        <InputRange
+          maxValue={600}
+          minValue={0}
+          formatLabel={value => `${value} mln €`}
+          value={this.state.value}
+          onChange={value => this.setState({ value })}
+          onChangeComplete={value => console.log(value)} />
 
         <ul className="teams-list__items">
           {this.props.items.toArray().map((item, key) => (
