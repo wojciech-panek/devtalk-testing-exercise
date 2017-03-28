35c0252ddb0931aded3ffb117bf3d6d5614c9320
Sergey Bolshov <bolszow@gmail.com>
2017-03-28 22:35:22 +0200

    Add team fetching error handling

--- app/modules/teams/teams.reducer.js
+++ app/modules/teams/teams.reducer.js
@@ -9,16 +9,19 @@ const StateRecord = new Record({
     min: 0,
     max: 600,
   }),
+  error: null,
 });
 
 const INITIAL_STATE = new StateRecord();
 
-const getSuccessHandler = (state = INITIAL_STATE, action) => state.set('list', fromJS(action.payload.teams));
-const setRangeValues = (state = INITIAL_STATE, action) => state.set('rangeValues', fromJS(action.values));
+const teamsSuccess = (state = INITIAL_STATE, action) => state.set('list', fromJS(action.payload.teams));
+const teamsError = (state = INITIAL_STATE, action) => state.set('error', action.payload.error);
+const rangeValuesSuccess = (state = INITIAL_STATE, action) => state.set('rangeValues', fromJS(action.values));
 
 const HANDLERS = {
-  [teamsActionsTypes.GET_TEAMS_SUCCESS]: getSuccessHandler,
-  [teamsActionsTypes.SET_RANGE_VALUES]: setRangeValues,
+  [teamsActionsTypes.GET_TEAMS_SUCCESS]: teamsSuccess,
+  [teamsActionsTypes.SET_RANGE_VALUES]: rangeValuesSuccess,
+  [teamsActionsTypes.GET_TEAMS_ERORR]: teamsError,
 };
 
 export default createReducer(INITIAL_STATE, HANDLERS);
